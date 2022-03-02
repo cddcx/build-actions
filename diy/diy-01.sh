5a861f94cc081cd45d7c9401cf7a6f34bb82895b
cddcx <84568016+cddcx@users.noreply.github.com>
2022-03-02 15:03:01 +0800

    Create diy-01.sh

---
+++ diy/diy-01.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+#
+
+# Uncomment a feed source
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+
+# 添加源
+echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+
+# luci-app-vssr及依赖
+git clone https://github.com/jerrykuku/lua-maxminddb.git package/lua-maxminddb
+git clone https://github.com/jerrykuku/luci-app-vssr.git package/luci-app-vssr
+
+# 软件中心istore
+svn co https://github.com/linkease/istore/trunk/luci/luci-app-store package/luci-app-store
+rm -rf package/luci-app-store/.svn
+sed -i 's/("iStore"), 31/("应用"), 61/g' package/luci-app-store/luasrc/controller/store.lua
+
+# luci-theme-opentopd主题
+git clone https://github.com/sirpdboy/luci-theme-opentopd package/luci-theme-opentopd
+rm -rf package/luci-theme-opentopd/README* package/luci-theme-opentopd/doc/
