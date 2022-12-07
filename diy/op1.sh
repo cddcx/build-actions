d04b2dd96dd0490f88f926abcc01aaeb0d1a2775
cddcx <84568016+cddcx@users.noreply.github.com>
2022-12-07 21:40:56 +0800

    Update op1.sh

--- diy/op1.sh
+++ diy/op1.sh
@@ -5,3 +5,4 @@
 #=================================================
 # 添加源
 echo "src-git xiangfeidexiaohuo https://github.com/xiangfeidexiaohuo/openwrt-packages" >>  feeds.conf.default
+https://github.com/coolsnowwolf/lede/trunk/package/lean package/lean
