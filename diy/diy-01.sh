d4a4348aaed9b05d8f0e5642528fcf0aaddae190
cddcx <84568016+cddcx@users.noreply.github.com>
2022-12-07 20:08:12 +0800

    Update diy-01.sh

--- diy/diy-01.sh
+++ diy/diy-01.sh
@@ -13,12 +13,18 @@
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
+#1. 默认ip
+#sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
+
+#2 修改密码
+#sed -i 's/root::0:0:99999:7:::/root:$1$SOP5eWTA$fJV8ty3QohO0chErhlxCm1:18775:0:99999:7:::/g' package/base-files/files/etc/shadow
+
 # 添加源
 echo "src-git xiangfeidexiaohuo https://github.com/xiangfeidexiaohuo/openwrt-packages" >>  feeds.conf.default
+svn co https://github.com/Lienol/openwrt-package/branches/other/lean ./package/lean #添加lean
 #echo 'src-git store https://github.com/linkease/istore.git;main' >>feeds.conf.default
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall" >> feeds.conf.default
-svn co https://github.com/Lienol/openwrt-package/branches/other/lean ./package/lean #添加lean 
+#echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall" >> feeds.conf.default 
 
 # luci-theme-opentopd主题
 git clone https://github.com/sirpdboy/luci-theme-opentopd package/luci-theme-opentopd
