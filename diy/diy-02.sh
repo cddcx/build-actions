a759609f71ff5237d7cbfb5830125cf0045490a9
cddcx <84568016+cddcx@users.noreply.github.com>
2022-12-07 20:04:53 +0800

    Update diy-02.sh

--- diy/diy-02.sh
+++ diy/diy-02.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-#1. 默认ip
-#sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
-
-#2 修改密码
-#sed -i 's/root::0:0:99999:7:::/root:$1$SOP5eWTA$fJV8ty3QohO0chErhlxCm1:18775:0:99999:7:::/g' package/base-files/files/etc/shadow
-
 #设置默认时区
 uci set system.@system[0].timezone=CST-8
 uci set system.@system[0].zonename=Asia/Shanghai
