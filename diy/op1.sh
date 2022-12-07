e27432b8821c044f8d15033f1f7e9911f3f3a4e6
cddcx <84568016+cddcx@users.noreply.github.com>
2022-12-07 21:28:52 +0800

    Update op1.sh

--- diy/op1.sh
+++ diy/op1.sh
@@ -3,5 +3,6 @@
 # DIY script
 # jsjson@163.com 
 #=================================================
-##添加自己的插件库
+# 添加源
 echo "src-git xiangfeidexiaohuo https://github.com/xiangfeidexiaohuo/openwrt-packages" >>  feeds.conf.default
+svn co https://github.com/Lienol/openwrt-package/branches/other/lean ./package/lean #添加lean
