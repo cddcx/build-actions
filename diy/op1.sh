bad3dce4503845e8387108ffb32df5595b686706
cddcx <84568016+cddcx@users.noreply.github.com>
2022-12-08 21:50:00 +0800

    Update op1.sh

--- diy/op1.sh
+++ diy/op1.sh
@@ -4,5 +4,5 @@
 # jsjson@163.com 
 #=================================================
 ##添加自己的插件库
-echo "src-git xiangfeidexiaohuo https://github.com/xiangfeidexiaohuo/openwrt-packages" >> feeds.conf.default
+echo "src-git xiangfeidexiaohuo https://github.com/xiangfeidexiaohuo/openwrt-packages" >>feeds.conf.default
 #sed -i "1isrc-git xiangfeidexiaohuo https://github.com/xiangfeidexiaohuo/openwrt-packages\n" feeds.conf.default
