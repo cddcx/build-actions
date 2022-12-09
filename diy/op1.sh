a6ccfb9a10ebba039691df345445a33385cdbd92
cddcx <84568016+cddcx@users.noreply.github.com>
2022-12-09 08:26:53 +0800

    Update op1.sh

--- diy/op1.sh
+++ diy/op1.sh
@@ -6,3 +6,7 @@
 ##添加自己的插件库
 echo 'src-git xiangfeidexiaohuo https://github.com/xiangfeidexiaohuo/openwrt-packages' >>feeds.conf.default
 #sed -i '1isrc-git xiangfeidexiaohuo https://github.com/xiangfeidexiaohuo/openwrt-packages\n' feeds.conf.default
+
+# luci-theme-opentopd主题
+git clone https://github.com/sirpdboy/luci-theme-opentopd package/luci-theme-opentopd
+rm -rf package/luci-theme-opentopd/README* package/luci-theme-opentopd/doc/
