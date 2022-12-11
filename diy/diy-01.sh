a290a0dc4af16b0c58019a15f63c21dd23d7aad1
cddcx <84568016+cddcx@users.noreply.github.com>
2022-12-11 20:14:53 +0800

    Update diy-01.sh

--- diy/diy-01.sh
+++ diy/diy-01.sh
@@ -20,15 +20,15 @@
 #sed -i 's/root::0:0:99999:7:::/root:$1$SOP5eWTA$fJV8ty3QohO0chErhlxCm1:18775:0:99999:7:::/g' package/base-files/files/etc/shadow
 
 # 添加源
-#echo 'src-git xiangfeidexiaohuo https://github.com/xiangfeidexiaohuo/openwrt-packages;master' >> feeds.conf.default
-echo 'src-git custom https://github.com/VergilGao/openwrt-packages;openwrt-21.02' >> feeds.conf.default
+echo 'src-git xiangfeidexiaohuo https://github.com/xiangfeidexiaohuo/openwrt-packages;master' >> feeds.conf.default
+#echo 'src-git custom https://github.com/VergilGao/openwrt-packages;openwrt-21.02' >> feeds.conf.default
 #echo 'src-git nas https://github.com/linkease/nas-packages.git;master' >> feeds.conf.default
 #echo 'src-git nas_luci https://github.com/linkease/nas-packages-luci.git;main' >> feeds.conf.default
 
- luci-app-openclash
-svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/luci-app-openclash
-rm -rf package/luci-app-openclash/.svn
+#luci-app-openclash
+#svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/luci-app-openclash
+#rm -rf package/luci-app-openclash/.svn
 
 # luci-theme-opentopd主题
-git clone https://github.com/sirpdboy/luci-theme-opentopd package/luci-theme-opentopd
-rm -rf package/luci-theme-opentopd/README* package/luci-theme-opentopd/doc/
+#git clone https://github.com/sirpdboy/luci-theme-opentopd package/luci-theme-opentopd
+#rm -rf package/luci-theme-opentopd/README* package/luci-theme-opentopd/doc/
