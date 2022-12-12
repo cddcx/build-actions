809ec9d389bf6be21333a8cdaa0a6d3e32926304
cddcx <84568016+cddcx@users.noreply.github.com>
2022-12-12 20:35:37 +0800

    Update diy-01.sh

--- diy/diy-01.sh
+++ diy/diy-01.sh
@@ -17,7 +17,7 @@
 #sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
 
 #2 修改密码
-#sed -i 's/root::0:0:99999:7:::/root:$1$SOP5eWTA$fJV8ty3QohO0chErhlxCm1:18775:0:99999:7:::/g' package/base-files/files/etc/shadow
+sed -i 's/root::0:0:99999:7:::/root:$1$xUooaZpA$6zs50xt4ac9sJXiYpycT3\/:19338:0:99999:7:::/g' package/base-files/files/etc/shadow
 
 # 添加源
 echo 'src-git xiangfeidexiaohuo https://github.com/xiangfeidexiaohuo/openwrt-packages;master' >> feeds.conf.default
