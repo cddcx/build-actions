4f52618980cac22209c0d7a2b4bacb1e93a0b26d
cddcx <84568016+cddcx@users.noreply.github.com>
2022-12-12 08:52:30 +0800

    Update diy-02.sh

--- diy/diy-02.sh
+++ diy/diy-02.sh
@@ -10,16 +10,19 @@ uci set system.@system[0].zonename=Asia/Shanghai
 
 # Docker v20.10.15
 rm -rf feeds/packages/utils/docker
-#rm -rf feeds/packages/utils/docker-compose
+rm -rf feeds/packages/utils/docker-compose
 rm -rf feeds/packages/utils/dockerd
 rm -rf feeds/packages/utils/containerd
+rm -rf feeds/packages/utils/coremark
 rm -rf feeds/packages/utils/runc
 rm -rf feeds/packages/utils/libnetwork
-#svn export https://github.com/coolsnowwolf/packages/trunk/utils/docker feeds/packages/utils/docker
-#svn export https://github.com/coolsnowwolf/packages/trunk/utils/dockerd feeds/packages/utils/dockerd
-#svn export https://github.com/coolsnowwolf/packages/trunk/utils/containerd feeds/packages/utils/containerd
-#svn export https://github.com/coolsnowwolf/packages/trunk/utils/runc feeds/packages/utils/runc
-#svn export https://github.com/coolsnowwolf/packages/trunk/utils/libnetwork feeds/packages/utils/libnetwork
+rm -rf package/feeds/luci/luci-app-dockerman
+rm -rf package/feeds/luci/luci-lib-docker
+rm -rf package/feeds/packages/docker-compose
+rm -rf package/feeds/packages/dockerd
+rm -rf feeds/xiangfeidexiaohuo/patch/autocore
+rm -rf feeds/packages/net/adguardhome
+rm -rf feeds/packages/net/smartdns
 
 ##配置IP
 #sed -i 's/192.168.1.1/192.168.100.101/g' package/base-files/files/bin/config_generate
@@ -51,18 +54,8 @@ ln -s ../../../feeds/luci/libs/luci-lib-fs package/feeds/xiangfeidexiaohuo/luci-
 #sed -i 's/LUCI_LANG.zh_Hans/LUCI_LANG.zh-cn/g' feeds/luci/luci.mk
 #sed -i 's/LUCI_LANG.zh_Hant/LUCI_LANG.zh-tw/g' feeds/luci/luci.mk
 sed -i "53iLUCI_LANG.zh-cn=\$(LUCI_LANG.zh_Hans)" feeds/luci/luci.mk
-sed -i "54iLUCI_LANG.zh-tw=\$(LUCI_LANG.zh_Hant)" feeds/luci/luci.mk
-
-##
-rm -rf package/feeds/luci/luci-app-dockerman
-#ln -s ../../../feeds/xiangfeidexiaohuo/lisaac/luci-app-dockerman package/feeds/luci/luci-app-dockerman
-rm -rf feeds/xiangfeidexiaohuo/patch/autocore
-#svn export https://github.com/Lienol/openwrt-package/branches/other/lean/autocore feeds/xiangfeidexiaohuo/patch/autocore 
-rm -rf feeds/packages/utils/coremark 
-#svn export https://github.com/coolsnowwolf/packages/trunk/utils/coremark feeds/packages/utils/coremark 
-rm -rf feeds/packages/net/adguardhome
-rm -rf feeds/packages/net/smartdns 
-
+sed -i "54iLUCI_LANG.zh-tw=\$(LUCI_LANG.zh_Hant)" feeds/luci/luci.mk 
+ 
 ##修改openwrt的include/target.mk文件
 sed -i 's/libustream-wolfssl/libustream-openssl/g' include/target.mk
 sed -i 's/dnsmasq/dnsmasq-full/g' include/target.mk
