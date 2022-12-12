024ef304ed430ab40dd3dfe2800d603ee5d53a57
cddcx <84568016+cddcx@users.noreply.github.com>
2022-12-12 17:14:10 +0800

    Update diy-02.sh

--- diy/diy-02.sh
+++ diy/diy-02.sh
@@ -44,8 +44,12 @@ mv package/lucimod/* feeds/luci/modules/luci-base/htdocs/luci-static/resources/i
 #sed -i "s/DISTRIB_REVISION='*.*'/DISTRIB_REVISION='$(date +%Y%m%d) by HiJwm'/g" package/base-files/files/etc/openwrt_release #默认为openwrt版本号，无个人信息
 #sed -i "s/DISTRIB_DESCRIPTION='*.*'/DISTRIB_DESCRIPTION='OpenWrt-$(date +%Y%m%d) by HiJwm'/g"  package/base-files/files/etc/openwrt_release #编译文件中添加，这个就无效了
 
-##把nas-packages-luci的zh-cn替换成zh_Hans 
-cp -af feeds/xiangfeidexiaohuo/linkease/nas-packages-luci/luci-app-quickstart/po/zh-cn feeds/xiangfeidexiaohuo/linkease/nas-packages-luci/luci-app-quickstart/po/zh_Hans 
+##把nas-packages-luci的zh-cn替换成zh_Hans
+sed -i 's/("QuickStart")/("首页")/g' feeds/xiangfeidexiaohuo/linkease/nas-packages-luci/luci-app-quickstart/luasrc/controller/quickstart.lua
+sed -i 's/("NetworkGuide")/("网络向导")/g' feeds/xiangfeidexiaohuo/linkease/nas-packages-luci/luci-app-quickstart/luasrc/controller/quickstart.lua
+sed -i 's/("RAID")/("磁盘阵列")/g' feeds/xiangfeidexiaohuo/linkease/nas-packages-luci/luci-app-quickstart/luasrc/controller/quickstart.lua
+sed -i 's/("NetworkPort")/("网口配置")/g' feeds/xiangfeidexiaohuo/linkease/nas-packages-luci/luci-app-quickstart/luasrc/controller/quickstart.lua
+#cp -af feeds/xiangfeidexiaohuo/linkease/nas-packages-luci/luci-app-quickstart/po/zh-cn feeds/xiangfeidexiaohuo/linkease/nas-packages-luci/luci-app-quickstart/po/zh_Hans 
  
 ##修改openwrt的include/target.mk文件
 sed -i 's/libustream-wolfssl/libustream-openssl/g' include/target.mk
