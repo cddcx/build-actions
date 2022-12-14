81271daac06a61b0f6ac0d4f33f0c35bfcd251a6
cddcx <84568016+cddcx@users.noreply.github.com>
2022-12-14 16:26:02 +0800

    Update diy-02.sh

--- diy/diy-02.sh
+++ diy/diy-02.sh
@@ -8,22 +8,21 @@
 #uci set system.@system[0].timezone=CST-8
 #uci set system.@system[0].zonename=Asia/Shanghai
 
-# Docker v20.10.15
-#rm -rf feeds/luci/applications/luci-app-adguardhome
+## 删除软件
 #rm -rf feeds/luci/applications/luci-app-disk*
 rm -rf feeds/luci/applications/luci-app-docker*
-rm -rf feeds/luci/applications/luci-app-smartdns
+rm -rf package/feeds/luci/luci-app-docker*
+rm -rf package/feeds/luci/luci-lib-docker
 rm -rf feeds/packages/utils/docker*
+rm -rf package/feeds/packages/docker*
+rm -rf feeds/luci/applications/luci-app-smartdns
+rm -rf feeds/packages/net/smartdns
 #rm -rf feeds/packages/utils/containerd
 #rm -rf feeds/packages/utils/coremark
 #rm -rf feeds/packages/utils/runc
 #rm -rf feeds/packages/utils/libnetwork
-rm -rf package/feeds/luci/luci-app-docker*
-rm -rf package/feeds/luci/luci-lib-docker
-rm -rf package/feeds/packages/docker*
+#rm -rf feeds/luci/applications/luci-app-adguardhome
 r#m -rf feeds/packages/net/adguardhome
-rm -rf feeds/packages/net/smartdns
-#rm -rf feeds/xiangfeidexiaohuo/patch/autocore
 
 ##配置IP
 #sed -i 's/192.168.1.1/192.168.100.101/g' package/base-files/files/bin/config_generate
@@ -32,7 +31,6 @@ rm -rf feeds/packages/net/smartdns
 sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci-nginx/Makefile
-
 ##替换theme icons
 #wget -O ./feeds/xiangfeidexiaohuo/jerrykuku/luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg https://github.com/HiJwm/MySettings/raw/main/BackGround/2.jpg
 #svn co https://github.com/xylz0928/luci-mod/trunk/feeds/luci/modules/luci-base/htdocs/luci-static/resources/icons ./package/lucimod
@@ -58,7 +56,7 @@ sed -i 's/dnsmasq/dnsmasq-full/g' include/target.mk
 sed -i 's/kmod-ipt-offload/block-mount coremark kmod-nf-nathelper kmod-nf-nathelper-extra kmod-ipt-raw kmod-tun dkmod-ipt-offload/g' include/target.mk
 sed -i 's/ip6tables/ip6tables iptables-mod-tproxy iptables-mod-extra ipset ip-full default-settings luci luci-newapi/g' include/target.mk
 sed -i 's/odhcp6c/odhcp6c ddns-scripts_aliyun ddns-scripts_dnspod/g' include/target.mk
-sed -i 's/odhcpd-ipv6only/odhcpd-ipv6only curl ca-certificates luci-app-udpxy/g' include/target.mk
+sed -i 's/odhcpd-ipv6only/odhcpd-ipv6only curl ca-certificates luci-app-istorex luci-app-udpxy luci-app-UPnP/g' include/target.mk
 
 ##修改openwrt/target/linux/x86的Makefile文件
 sed -i 's/kmod-button-hotplug/kmod-alx kmod-e1000e kmod-igb kmod-igc kmod-igbvf kmod-iavf kmod-bnx2x kmod-pcnet32 kmod-tulip kmod-via-velocity kmod-vmxnet3 kmod-i40e kmod-i40evf kmod-r8125 kmod-8139cp kmod-8139too kmod-tg3 htop lm-sensors iperf3 ca-bundle kmod-sound-hda-core kmod-sound-hda-codec-realtek kmod-sound-hda-codec-via kmod-sound-via82xx kmod-sound-hda-intel kmod-sound-hda-codec-hdmi kmod-sound-i8x0 kmod-usb-audio kmod-usb-net kmod-usb-net-asix-ax88179 kmod-usb-net-rtl8150 kmod-usb-net-rtl8152-vendor kmod-usb-net-aqc111 kmod-mlx4-core kmod-mlx5-core kmod-drm-i915 kmod-drm-amdgpu/g' target/linux/x86/Makefile
