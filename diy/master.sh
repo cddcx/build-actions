abfd07978e40e1632ad081038d7aae010eed7547
cddcx <84568016+cddcx@users.noreply.github.com>
2022-12-16 20:37:25 +0800

    Update master.sh

--- diy/master.sh
+++ diy/master.sh
@@ -16,11 +16,9 @@ sed -i 's/firewall4/firewall4 block-mount coremark/g' include/target.mk
 sed -i 's/nftables/nftables curl ca-certificates/g' include/target.mk
 sed -i 's/odhcp6c/odhcp6c iptables-mod-tproxy iptables-mod-extra ipset ip-full default-settings luci luci-newapi/g' include/target.mk
 sed -i 's/odhcpd-ipv6only/odhcpd-ipv6only ddns-scripts_aliyun ddns-scripts_dnspod/g' include/target.mk
-sed -i 's/ppp-mod-pppoe/luci-app-filetransfer luci-app-openclash luci-app-istorex luci-app-udpxy luci-app-upnp/g' include/target.mk
-sed -i 's/ppp/ppp  ppp-mod-pppoe/g' include/target.mk
 
 ##修改openwrt/target/linux/x86的Makefile文件
-#sed -i 's/DEFAULT_PACKAGES +=/DEFAULT_PACKAGES += kmod-usb-hid kmod-mmc kmod-sdhci kmod-fs-f2fs cfdisk usbutils pciutils kmod-mlx4-core kmod-mlx5-core kmod-drm-i915 kmod-drm-amdgpu/g' target/linux/x86/Makefile
-#sed -i 's/partx-utils mkf2fs/partx-utils mkf2fs kmod-alx kmod-e1000e kmod-igb kmod-igc kmod-igbvf kmod-iavf kmod-bnx2x kmod-pcnet32 kmod-tulip kmod-via-velocity kmod-vmxnet3 kmod-i40e kmod-i40evf kmod-r8125 kmod-8139cp kmod-8139too kmod-tg3/g' target/linux/x86/Makefile
-#sed -i 's/e2fsprogs kmod-button-hotplug/e2fsprogs kmod-button-hotplug htop lm-sensors iperf3 autocore-x86 kmod-usb-net kmod-usb-net-asix-ax88179 kmod-usb-net-rtl8150 kmod-usb-net-rtl8152-vendor kmod-usb-net-aqc111/g' target/linux/x86/Makefile
-#sed -i 's/grub2-bios-setup/kmod-sound-hda-core kmod-sound-hda-codec-realtek kmod-sound-hda-codec-via kmod-sound-via82xx kmod-sound-hda-intel kmod-sound-hda-codec-hdmi kmod-sound-i8x0 kmod-usb-audio/g' target/linux/x86/Makefile
+sed -i 's/DEFAULT_PACKAGES +=/DEFAULT_PACKAGES += kmod-usb-hid kmod-mmc kmod-sdhci kmod-fs-f2fs cfdisk usbutils pciutils kmod-mlx4-core kmod-mlx5-core kmod-drm-i915 kmod-drm-amdgpu kmod-sound-hda-intel kmod-sound-hda-codec-hdmi kmod-sound-i8x0 kmod-usb-audio/g' target/linux/x86/Makefile
+sed -i 's/partx-utils mkf2fs/partx-utils mkf2fs kmod-alx kmod-e1000e kmod-igb kmod-igc kmod-igbvf kmod-iavf kmod-bnx2x kmod-pcnet32 kmod-tulip kmod-via-velocity kmod-vmxnet3 kmod-i40e kmod-i40evf kmod-r8125 kmod-8139cp kmod-8139too kmod-tg3/g' target/linux/x86/Makefile
+sed -i 's/e2fsprogs kmod-button-hotplug/e2fsprogs kmod-button-hotplug htop lm-sensors iperf3 autocore-x86 kmod-usb-net kmod-usb-net-asix-ax88179 kmod-usb-net-rtl8150 kmod-usb-net-rtl8152-vendor kmod-usb-net-aqc111/g' target/linux/x86/Makefile
+sed -i 's/grub2-bios-setup/kmod-sound-hda-core kmod-sound-hda-codec-realtek kmod-sound-hda-codec-via kmod-sound-via82xx luci-app-filetransfer luci-app-openclash luci-app-istorex luci-app-udpxy luci-app-upnp/g' target/linux/x86/Makefile
