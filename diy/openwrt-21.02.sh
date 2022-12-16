0c1160819d49dffc13d0da8964cbdf4c1ba961ec
cddcx <84568016+cddcx@users.noreply.github.com>
2022-12-16 19:44:58 +0800

    Update and rename diy-01.sh to openwrt-21.02.sh

--- diy/openwrt-21.02.sh
+++ diy/openwrt-21.02.sh
@@ -19,19 +19,16 @@
 #2 修改密码
 sed -i 's/root::0:0:99999:7:::/root:$1$xUooaZpA$6zs50xt4ac9sJXiYpycT3\/:19338:0:99999:7:::/g' package/base-files/files/etc/shadow
 
-# 添加源
+## 添加源和软件
 #echo 'src-git xiangfeidexiaohuo https://github.com/xiangfeidexiaohuo/openwrt-packages;master' >> feeds.conf.default
 svn co https://github.com/xiangfeidexiaohuo/openwrt-packages/trunk/linkease package/linkease
 svn co https://github.com/xiangfeidexiaohuo/openwrt-packages/trunk/jerrykuku/luci-theme-argon package/luci-theme-argon
-
 # luci-app-openclash
 svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/luci-app-openclash
 rm -rf package/luci-app-openclash/.svn
-
-##添加终端(luci-app-ttyd)
+## 添加终端(luci-app-ttyd)
 #svn co https://github.com/coolsnowwolf/luci/trunk/applications/luci-app-ttyd package/luci-app-ttyd
 #cp -af package/luci-app-ttyd/po/zh-cn package/luci-app-ttyd/po/zh_Hans
-
 ##添加文件传输(filetransert)
 #svn export https://github.com/coolsnowwolf/luci/trunk/libs/luci-lib-fs package/add/luci-lib-fs
 #svn export https://github.com/coolsnowwolf/luci/trunk/applications/luci-app-filetransfer package/add/luci-app-filetransfer
