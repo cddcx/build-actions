e43d6b2f265a8fce477624500d3f4175994bf2b9
cddcx <84568016+cddcx@users.noreply.github.com>
2022-12-26 20:17:40 +0800

    Update 22.03.sh

--- diy/22.03.sh
+++ diy/22.03.sh
@@ -31,9 +31,13 @@ rm -rf package/luci-app-openclash/.svn
 
 ##修改openwrt的include/target.mk文件
 sed -i 's/libustream-wolfssl/libustream-openssl/g' include/target.mk
-sed -i 's/nftables/iptables ip6tables/g' include/target.mk
-sed -i 's/kmod-nft-offload/kmod-ipt-offload/g' include/target.mk
-sed -i 's/dnsmasq/dnsmasq-full block-mount coremark kmod-nf-nathelper kmod-nf-nathelper-extra kmod-ipt-raw kmod-tun iptables-mod-tproxy iptables-mod-extra ipset ip-full default-settings luci curl ca-certificates/g' include/target.mk
+sed -i 's/dnsmasq/dnsmasq-full/g' include/target.mk
+sed -i 's/nftables/nftables default-settings luci luci-compat/g' include/target.mk
+##
+#sed -i 's/libustream-wolfssl/libustream-openssl/g' include/target.mk
+#sed -i 's/nftables/iptables ip6tables/g' include/target.mk
+#sed -i 's/kmod-nft-offload/kmod-ipt-offload/g' include/target.mk
+#sed -i 's/dnsmasq/dnsmasq-full block-mount coremark kmod-nf-nathelper kmod-nf-nathelper-extra kmod-ipt-raw kmod-tun iptables-mod-tproxy iptables-mod-extra ipset ip-full default-settings luci curl ca-certificates/g' include/target.mk
 
 ##修改openwrt/target/linux/x86的Makefile文件
 sed -i 's/mkf2fs/mkf2fs mod-usb-hid kmod-mmc kmod-sdhci kmod-fs-f2fs cfdisk usbutils pciutils kmod-alx kmod-e1000e kmod-igb kmod-igc kmod-igbvf kmod-iavf kmod-bnx2x kmod-pcnet32 kmod-tulip kmod-via-velocity kmod-vmxnet3 kmod-i40e kmod-i40evf kmod-r8125 kmod-8139cp kmod-8139too kmod-tg3 htop lm-sensors iperf3 autocore-x86 kmod-sound-hda-core kmod-sound-hda-codec-realtek kmod-sound-hda-codec-via kmod-sound-via82xx kmod-sound-hda-intel kmod-sound-hda-codec-hdmi kmod-sound-i8x0 kmod-usb-audio kmod-usb-net kmod-usb-net-asix-ax88179 kmod-usb-net-rtl8150 kmod-usb-net-rtl8152-vendor kmod-usb-net-aqc111 kmod-mlx4-core kmod-mlx5-core kmod-drm-i915 kmod-drm-amdgpu luci-app-openclash luci-app-istorex luci-app-udpxy luci-app-upnp/g' target/linux/x86/Makefile
