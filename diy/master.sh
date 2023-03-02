74bd642ccb78781498e92482d0677d01ea92e040
cddcx <84568016+cddcx@users.noreply.github.com>
2023-03-02 21:22:05 +0800

    Update master.sh

--- diy/master.sh
+++ diy/master.sh
@@ -6,6 +6,7 @@ sed -i 's/root:::0:99999:7:::/root:$1$xUooaZpA$6zs50xt4ac9sJXiYpycT3\/:19338:0:9
 #echo 'src-git-full packages https://github.com/kiddin9/openwrt-packages.git;master' >> feeds.conf.default
 #echo 'src-git xiangfeidexiaohuo https://github.com/xiangfeidexiaohuo/openwrt-packages;master' >> feeds.conf.default
 svn co https://github.com/xiangfeidexiaohuo/openwrt-packages/trunk/linkease package/linkease
+rm -rf package/linkease/preview
 svn co https://github.com/xiangfeidexiaohuo/openwrt-packages/trunk/jerrykuku/luci-theme-argon package/luci-theme-argon
 
 # luci-app-openclash
