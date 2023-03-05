76a8bad3065e840170f6519c67634cda215247de
cddcx <84568016+cddcx@users.noreply.github.com>
2023-03-05 08:05:58 +0800

    Update master.sh

--- diy/master.sh
+++ diy/master.sh
@@ -15,7 +15,7 @@ rm -rf package/luci-app-openclash/.svn
 
 ## 修改openwrt的include/target.mk文件
 sed -i 's/dnsmasq/dnsmasq-full/g' include/target.mk
-sed -i "s/DEFAULT_PACKAGES:=/DEFAULT_PACKAGES:=luci-app-firewall luci-app-opkg luci-base luci-compat luci-lib-ipkg luci-lib-fs default-settings luci \
+sed -i "s/DEFAULT_PACKAGES:=/DEFAULT_PACKAGES:=luci-app-firewall luci-app-opkg luci-base luci-compat luci-lib-ipkg luci-lib-fs default-settings-chn luci \
 coremark wget-ssl curl htop nano zram-swap kmod-lib-zstd kmod-tcp-bbr bash openssh-sftp-server block-mount resolveip ds-lite swconfig /" include/target.mk
 
 ##修改openwrt/target/linux/x86的Makefile文件
