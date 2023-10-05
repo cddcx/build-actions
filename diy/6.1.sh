aee164d83e1f7197879343a68910230469b25dc9
cddcx <84568016+cddcx@users.noreply.github.com>
2023-10-05 20:01:00 +0800

    Update 6.1.sh

--- diy/6.1.sh
+++ diy/6.1.sh
@@ -25,8 +25,7 @@ rm -rf package/luci-app-openclash/.svn
 
 ## 修改openwrt的include/target.mk文件
 curl -sfL https://raw.githubusercontent.com/immortalwrt/immortalwrt/master/include/target.mk -o include/target.mk
-#sed -i "s/DEFAULT_PACKAGES:=/DEFAULT_PACKAGES:=luci-app-firewall luci-app-opkg luci-app-udpxy luci-app-upnp luci-app-openclash \
-luci-base luci-compat ipset luci-lib-ipkg luci-lib-fs \
-coremark wget-ssl curl autocore htop nano zram-swap kmod-lib-zstd kmod-tcp-bbr bash luci \
-openssh-sftp-server block-mount resolveip ds-lite swconfig /" include/target.mk
-sed -i "s/procd-ujail//" include/target.mk
+
+## 修改openwrt/target/linux/x86的Makefile文件
+curl -sfL https://raw.githubusercontent.com/immortalwrt/immortalwrt/master/target/linux/x86/Makefile -o target/linux/x86/Makefile
+sed -i 's/automount/autocore default-settings ipset luci luci-compat luci-app-udpxy luci-app-upnp luci-app-openclash/g' target/linux/x86/Makefile
