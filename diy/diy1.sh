61cf08c1fdc951da6cdfe4a7b233b10f05a8046f
cddcx <84568016+cddcx@users.noreply.github.com>
2023-11-29 20:22:05 +0800

    Update and rename 21.02.sh to diy1.sh

---
+++ diy/diy1.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+#
+
+# Uncomment a feed source
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+
+#1. 默认ip
+#sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
+
+#2 修改密码
+sed -i 's/root::0:0:99999:7:::/root:$1$pfsE8FKB$tnZcDcV8vUTqxJpwXLzZv1:19690:0:99999:7:::/g' package/base-files/files/etc/shadow
+
+## 添加源和软件
+# luci-app-filetransfer
+svn co https://github.com/immortalwrt/luci/trunk/applications/luci-app-filetransfer package/luci-app-filetransfer
+rm -rf package/luci-app-filetransfer/.svn
+
+# luci-app-homeproxy
+git clone https://github.com/immortalwrt/homeproxy package/homeproxy
+
+# luci-app-passwall2
+git clone https://github.com/xiaorouji/openwrt-passwall-packages package/passwall
+git clone https://github.com/xiaorouji/openwrt-passwall2 package/luci-app-passwall2
