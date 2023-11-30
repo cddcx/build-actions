8627f833342268c3b8c2e020fdd4715434e256cb
cddcx <84568016+cddcx@users.noreply.github.com>
2023-11-30 12:36:55 +0800

    Update diy1.sh

--- diy/diy1.sh
+++ diy/diy1.sh
@@ -17,14 +17,14 @@
 #sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
 
 #2 修改密码
-sed -i 's/root::0:0:99999:7:::/root:$1$pfsE8FKB$tnZcDcV8vUTqxJpwXLzZv1:19690:0:99999:7:::/g' package/base-files/files/etc/shadow
+#sed -i 's/root::0:0:99999:7:::/root:$1$pfsE8FKB$tnZcDcV8vUTqxJpwXLzZv1:19690:0:99999:7:::/g' package/base-files/files/etc/shadow
 
 ## 添加源和软件
 # luci-app-filetransfer
 #svn co https://github.com/immortalwrt/luci/trunk/applications/luci-app-filetransfer package/luci-app-filetransfer
 #rm -rf package/luci-app-filetransfer/.svn
-svn co https://github.com/sbwml/openwrt_pkgs/trunk/filebrowser package/filebrowser
-svn co https://github.com/sbwml/openwrt_pkgs/trunk/luci-app-filebrowser package/filebrowser
+#svn co https://github.com/sbwml/openwrt_pkgs/trunk/filebrowser package/filebrowser
+#svn co https://github.com/sbwml/openwrt_pkgs/trunk/luci-app-filebrowser package/filebrowser
 
 # luci-app-homeproxy
 git clone https://github.com/immortalwrt/homeproxy package/homeproxy
