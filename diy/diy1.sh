f52f76066d919ddd3f5dd2c38f492ca4f75e3921
cddcx <84568016+cddcx@users.noreply.github.com>
2023-11-30 12:57:37 +0800

    Update diy1.sh

--- diy/diy1.sh
+++ diy/diy1.sh
@@ -36,7 +36,7 @@ git clone https://github.com/xiaorouji/openwrt-passwall2 package/luci-app-passwa
 ## 修改target.mk
 sed -i 's/dnsmasq/dnsmasq-full/g' include/target.mk
 sed -i "s/kmod-nft-offload/kmod-nft-offload kmod-nft-tproxy/" include/target.mk
-sed -i "s/DEFAULT_PACKAGES.router:=/DEFAULT_PACKAGES.router:=curl luci luci-light luci-compat luci-lib-base luci-lib-ipkg /
+sed -i "s/DEFAULT_PACKAGES.router:=/DEFAULT_PACKAGES.router:=curl luci luci-light luci-compat luci-lib-base luci-lib-ipkg \
 luci-app-opkg luci-app-firewall/" include/target.mk
 
 # 修改target/linux/x86/Makefile
