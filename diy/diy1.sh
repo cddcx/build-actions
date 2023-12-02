888767e6ba1105f13d3b3a32548e62462565ed23
cddcx <84568016+cddcx@users.noreply.github.com>
2023-12-02 21:47:49 +0800

    Update diy1.sh

--- diy/diy1.sh
+++ diy/diy1.sh
@@ -13,7 +13,6 @@
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
-##### 添加源和软件
 ## luci-app-filetransfer
 svn co https://github.com/immortalwrt/luci/trunk/applications/luci-app-filetransfer package/luci-app-filetransfer
 rm -rf package/luci-app-filetransfer/.svn
