660928e286b5ae676983e8384a6c181bec45f2cd
cddcx <84568016+cddcx@users.noreply.github.com>
2023-12-03 19:26:51 +0800

    Update diy1.sh

--- diy/diy1.sh
+++ diy/diy1.sh
@@ -31,8 +31,8 @@ git clone https://github.com/v2rayA/v2raya-openwrt package/v2raya-openwrt
 
 ## luci-app-homeproxy
 git clone https://github.com/immortalwrt/homeproxy package/homeproxy
-sed -i "s/ImmortalWrt/OpenWrt/g" package/homeproxy/po/zh_Hans/homeproxy.po
-sed -i "s/ImmortalWrt proxy/OpenWrt proxy/g" package/homeproxy/htdocs/luci-static/resources/view/homeproxy/{client.js,server.js}
+sed -i "s@ImmortalWrt@OpenWrt@g" package/homeproxy/po/zh_Hans/homeproxy.po
+sed -i "s@ImmortalWrt proxy@OpenWrt proxy@g" package/homeproxy/htdocs/luci-static/resources/view/homeproxy/{client.js,server.js}
 
 ## luci-app-passwall2
 git clone https://github.com/xiaorouji/openwrt-passwall-packages package/passwall
