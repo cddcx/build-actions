e3941317afd35aa9c684ef20d5e27e1b1433600a
cddcx <84568016+cddcx@users.noreply.github.com>
2023-12-05 08:30:53 +0800

    Update diy2.sh

--- diy/diy2.sh
+++ diy/diy2.sh
@@ -8,7 +8,7 @@
 sed -i 's/PATCHVER:=*.*/PATCHVER:=6.1/g' target/linux/x86/Makefile 
 
 ##. 默认ip
-#sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
+#sed -i 's/*.*.*.*/192.168.1.1/g' package/base-files/files/bin/config_generate
 
 ## 修改密码
 #sed -i 's@root:::0:99999:7:::@root:$1$pfsE8FKB$tnZcDcV8vUTqxJpwXLzZv1:19690:0:99999:7:::@g' package/base-files/files/etc/shadow
