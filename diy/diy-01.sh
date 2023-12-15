9f2da7284b1e69d3e5ba852e7673453568c38225
cddcx <84568016+cddcx@users.noreply.github.com>
2023-12-15 16:12:38 +0800

    Create diy-01.sh

---
+++ diy/diy-01.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+#
+
+# Uncomment a feed source
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+
+## default-settings
+#mkdir -p package/emortal/default-settings
+#git_clone_path master https://github.com/immortalwrt/immortalwrt package/emortal/default-settings
+git clone https://github.com/cddcx/default-settings.git package/emortal/default-settings
+
+## luci-app-filetransfer
+#git clone https://github.com/cddcx/luci-app-filetransfer.git package/luci-app-filetransfer
+#sed -i 's@../../luci.mk@$(TOPDIR)/feeds/luci/luci.mk@g' package/luci-app-filetransfer/Makefile
+# luci-app-filetransfer依赖luci-lib-fs
+#git clone https://github.com/cddcx/luci-lib-fs.git package/luci-lib-fs
+
+## luci-app-v2raya
+#git clone https://github.com/v2rayA/v2raya-openwrt package/v2raya-openwrt
+#rm -rf package/v2raya-openwrt/{v2raya,xray-core}
+
+## luci-app-homeproxy
+#git clone https://github.com/immortalwrt/homeproxy package/homeproxy           ####### homeproxy的默认版本(二选一)
+#git clone -b dev https://github.com/immortalwrt/homeproxy package/homeproxy     ####### homeproxy的dev版本(二选一)  
+#sed -i "s@ImmortalWrt@OpenWrt@g" package/homeproxy/po/zh_Hans/homeproxy.po
+#sed -i "s@ImmortalWrt proxy@OpenWrt proxy@g" package/homeproxy/htdocs/luci-static/resources/view/homeproxy/{client.js,server.js}
+
+## luci-app-passwall2
+git clone https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwall
+git clone https://github.com/xiaorouji/openwrt-passwall-packages package/passwall
+
+## luci-app-openclash
+#git clone https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/luci-app-openclash
