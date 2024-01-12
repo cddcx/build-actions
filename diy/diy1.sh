e56683a3312d0ff5c2f77e13a38c19a63ae885a4
cddcx <84568016+cddcx@users.noreply.github.com>
2024-01-12 09:14:25 +0800

    Update diy1.sh

--- diy/diy1.sh
+++ diy/diy1.sh
@@ -37,8 +37,11 @@ git_clone_path master https://github.com/immortalwrt/immortalwrt package/emortal
 ## 磁盘管理luci-app-diskman及依赖parted
 #git clone https://github.com/lisaac/luci-app-diskman package/luci-app-diskman
 #mkdir -p package/parted && cp -i package/luci-app-diskman/Parted.Makefile package/parted/Makefile
+mkdir -p package/luci-app-diskman
 mkdir -p applications/luci-app-diskman
 git_clone_path master https://github.com/immortalwrt/luci applications/luci-app-diskman
+cp -i applications/luci-app-diskman package/luci-app-diskman
+rm -rf applications/luci-app-diskman
 
 ## luci-app-filetransfer
 #git clone https://github.com/cddcx/luci-app-filetransfer.git package/luci-app-filetransfer
