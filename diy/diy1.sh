dac20446908a7f98a44aec40e3babcc41b2eb9d7
cddcx <84568016+cddcx@users.noreply.github.com>
2024-01-13 09:23:41 +0800

    Update diy1.sh

--- diy/diy1.sh
+++ diy/diy1.sh
@@ -27,23 +27,23 @@ git_clone_path master https://github.com/immortalwrt/immortalwrt package/emortal
 ## docker
 mkdir -p utils/docker
 git_clone_path master https://github.com/immortalwrt/packages utils/docker
-#cp -rf utils/docker package/docker
+cp -rf utils/docker package/docker
 mkdir -p utils/dockerd
 git_clone_path master https://github.com/immortalwrt/packages utils/dockerd
-#cp -rf utils/dockerd package/dockerd
+cp -rf utils/dockerd package/dockerd
 mkdir -p utils/docker-compose
 git_clone_path master https://github.com/immortalwrt/packages utils/docker-compose
-#cp -rf utils/docker-compose package/docker-compose
+cp -rf utils/docker-compose package/docker-compose
 mkdir -p applications/luci-app-dockerman
 #git_clone_path master https://github.com/immortalwrt/luci applications/luci-app-dockerman
 git_clone_path master https://github.com/lisaac/luci-app-dockerman applications/luci-app-dockerman
-#cp -rf applications/luci-app-dockerman package/luci-app-dockerman
-#rm -rf applications
-#rm -rf utils
-#sed -i 's@../../lang@$(TOPDIR)/feeds/packages/lang@g' package/{docker,docker-compose,dockerd}/Makefile
+cp -rf applications/luci-app-dockerman package/luci-app-dockerman
+rm -rf applications
+rm -rf utils
+sed -i 's@../../luci.mk@$(TOPDIR)/feeds/luci/luci.mk@g' package/luci-app-dockerman/Makefile
+sed -i 's@../../lang@$(TOPDIR)/feeds/packages/lang@g' package/{docker,docker-compose,dockerd}/Makefile
 #sed -i 's@../../lang@$(TOPDIR)/feeds/packages/lang@g' package/docker-compose/Makefile
 #sed -i 's@../../lang@$(TOPDIR)/feeds/packages/lang@g' package/dockerd/Makefile
-#sed -i 's@../../luci.mk@$(TOPDIR)/feeds/luci/luci.mk@g' package/luci-app-dockerman/Makefile
 
 ## 磁盘管理luci-app-diskman及依赖parted
 #git clone https://github.com/lisaac/luci-app-diskman package/luci-app-diskman
