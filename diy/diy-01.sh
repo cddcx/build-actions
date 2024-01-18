6e8e207791217d2c76cfdbebb2777ef20115c51d
cddcx <84568016+cddcx@users.noreply.github.com>
2024-01-18 14:54:36 +0800

    Update diy-01.sh

--- diy/diy-01.sh
+++ diy/diy-01.sh
@@ -39,11 +39,11 @@ git clone https://github.com/cddcx/default-settings.git package/emortal/default-
 #sed -i "s@ImmortalWrt proxy@OpenWrt proxy@g" package/homeproxy/htdocs/luci-static/resources/view/homeproxy/{client.js,server.js}
 
 ## luci-app-passwall
-#git_clone_path main https://github.com/xiaorouji/openwrt-passwall luci-app-passwall
-#cp -rf luci-app-passwall package/luci-app-passwall
-#rm -rf luci-app-passwall
+git_clone_path main https://github.com/xiaorouji/openwrt-passwall luci-app-passwall
+cp -rf luci-app-passwall package/luci-app-passwall
+rm -rf luci-app-passwall
 #svn checkout https://github.com/xiaorouji/openwrt-passwall/trunk/luci-app-passwall package/luci-app-passwall
-#git clone https://github.com/xiaorouji/openwrt-passwall-packages package/passwall
+git clone https://github.com/xiaorouji/openwrt-passwall-packages package/passwall
 
 ## luci-app-openclash
 #git clone https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/luci-app-openclash
