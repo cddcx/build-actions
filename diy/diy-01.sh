660741e2069f0bae6a967ef1bd06bfe3f720e4ba
cddcx <84568016+cddcx@users.noreply.github.com>
2024-01-18 19:19:59 +0800

    Update diy-01.sh

--- diy/diy-01.sh
+++ diy/diy-01.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 #=================================================
-#shopt -s extglob
+shopt -s extglob
 
-#function git_clone_path() {
-          #branch="$1" rurl="$2" localdir="gitemp" && shift 2
-          #git clone -b $branch --depth 1 --filter=blob:none --sparse $rurl $localdir
-          #if [ "$?" != 0 ]; then
-            #echo "error on $rurl"
-            #return 0
-          #fi
-          #cd $localdir
-          #git sparse-checkout init --cone
-          #git sparse-checkout set $@
-		  #mv -n $@/* ../$@/ || cp -rf $@ ../$(dirname "$@")/
-          #cd ..
-		  #rm -rf gitemp
-          #}
+function git_clone_path() {
+          branch="$1" rurl="$2" localdir="gitemp" && shift 2
+          git clone -b $branch --depth 1 --filter=blob:none --sparse $rurl $localdir
+          if [ "$?" != 0 ]; then
+            echo "error on $rurl"
+            return 0
+          fi
+          cd $localdir
+          git sparse-checkout init --cone
+          git sparse-checkout set $@
+		  mv -n $@/* ../$@/ || cp -rf $@ ../$(dirname "$@")/
+          cd ..
+		  rm -rf gitemp
+          }
 
 ## default-settings
 #mkdir -p package/emortal/default-settings
