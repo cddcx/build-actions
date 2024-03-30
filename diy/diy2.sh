3cfd87b0a57e7751fc40a025081a7547598fda4d
cddcx <84568016+cddcx@users.noreply.github.com>
2024-03-30 18:26:37 +0800

    Update diy2.sh

--- diy/diy2.sh
+++ diy/diy2.sh
@@ -59,5 +59,5 @@ rm -rf feeds/packages/net/{xray-core,v2ray-core,v2ray-geodata,sing-box}
 #git clone https://github.com/sbwml/feeds_packages_net_curl feeds/packages/net/curl
 #rm -rf feeds/packages/lang/golang
 #git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
-#rm -rf feeds/packages/lang/golang
-#git clone https://github.com/sbwml/packages_lang_golang -b 22.x feeds/packages/lang/golang
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 22.x feeds/packages/lang/golang
