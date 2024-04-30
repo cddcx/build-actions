267d407c3ecd246b8cd86956433a57bd07889e91
cddcx <84568016+cddcx@users.noreply.github.com>
2024-04-30 18:54:29 +0800

    Update diy2.sh

--- diy/diy2.sh
+++ diy/diy2.sh
@@ -63,4 +63,4 @@ rm -rf feeds/packages/lang/golang
 git clone https://github.com/sbwml/packages_lang_golang -b 22.x feeds/packages/lang/golang
 
 rm -rf feeds/packages/net/shadowsocks-libev
-cp -rf package/helloworld/shadowsocks-libev feeds/packages/net/shadowsocks-libev
+#cp -rf package/helloworld/shadowsocks-libev feeds/packages/net/shadowsocks-libev
