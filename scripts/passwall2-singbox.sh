93639d1c9b465ecded5cbfbdf735da281267d5c1
cddcx <84568016+cddcx@users.noreply.github.com>
2024-05-02 20:12:03 +0800

    Update passwall2-singbox.sh

--- scripts/passwall2-singbox.sh
+++ scripts/passwall2-singbox.sh
@@ -5,7 +5,7 @@ mkdir -p files/usr/share/singbox
 GEOIP_URL="https://github.com/MetaCubeX/meta-rules-dat/releases/download/latest/geoip.db"
 GEOSITE_URL="https://github.com/MetaCubeX/meta-rules-dat/releases/download/latest/geosite.dbt"
 
-wget -qO- $GEOIP_URL > ffiles/usr/share/singbox/GeoIP.dat
-wget -qO- $GEOSITE_URL > files/usr/share/singbox/GeoSite.dat
+wget -qO- $GEOIP_URL > files/usr/share/singbox/geoip.dat
+wget -qO- $GEOSITE_URL > files/usr/share/singbox/geosite.dat
 
 chmod +x ffiles/usr/share/singbox/geo*
