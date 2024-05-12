b79b041c53c149d164d2903fedb7fa95be0a3b55
cddcx <84568016+cddcx@users.noreply.github.com>
2024-05-12 19:11:10 +0800

    Update diy1.sh

--- diy/diy1.sh
+++ diy/diy1.sh
@@ -8,54 +8,8 @@ chmod +x ${GITHUB_WORKSPACE}/subscript.sh
 source ${GITHUB_WORKSPACE}/subscript.sh
 
 ## autocore automount default-settings
-#mkdir -p package/emortal
-#git_clone_path master https://github.com/immortalwrt/immortalwrt package/emortal
-#git_clone_path master https://github.com/immortalwrt/immortalwrt package/emortal/autocore
-#mkdir -p package/emortal/automount
-#git_clone_path master https://github.com/immortalwrt/immortalwrt package/emortal/automount
-#mkdir -p package/emortal/default-settings
 merge_package master https://github.com/immortalwrt/immortalwrt package/emortal package/emortal/default-settings
 #git clone https://github.com/cddcx/default-settings.git package/emortal/default-settings
-
-## docker
-#mkdir -p utils/docker
-#git_clone_path master https://github.com/immortalwrt/packages utils/docker
-#cp -rf utils/docker package/docker
-#mkdir -p utils/dockerd
-#git_clone_path master https://github.com/immortalwrt/packages utils/dockerd
-#cp -rf utils/dockerd package/dockerd
-#mkdir -p utils/docker-compose
-#git_clone_path master https://github.com/immortalwrt/packages utils/docker-compose
-#cp -rf utils/docker-compose package/docker-compose
-#mkdir -p applications/luci-app-dockerman
-#git_clone_path master https://github.com/immortalwrt/luci applications/luci-app-dockerman
-#git_clone_path master https://github.com/lisaac/luci-app-dockerman applications/luci-app-dockerman
-#cp -rf applications/luci-app-dockerman package/luci-app-dockerman
-#rm -rf applications
-#rm -rf utils
-#sed -i 's@../../luci.mk@$(TOPDIR)/feeds/luci/luci.mk@g' package/luci-app-dockerman/Makefile
-#sed -i 's@../../lang@$(TOPDIR)/feeds/packages/lang@g' package/{docker,docker-compose,dockerd}/Makefile
-#sed -i 's@../../lang@$(TOPDIR)/feeds/packages/lang@g' package/docker-compose/Makefile
-#sed -i 's@../../lang@$(TOPDIR)/feeds/packages/lang@g' package/dockerd/Makefile
-
-## 磁盘管理luci-app-diskman及依赖parted
-#git clone https://github.com/lisaac/luci-app-diskman package/luci-app-diskman
-#mkdir -p package/parted && cp -i package/luci-app-diskman/Parted.Makefile package/parted/Makefile
-#mkdir -p applications/luci-app-diskman
-#git_clone_path master https://github.com/immortalwrt/luci applications/luci-app-diskman
-#cp -rf applications/luci-app-diskman package/luci-app-diskman
-#rm -rf applications
-#sed -i 's@../../luci.mk@$(TOPDIR)/feeds/luci/luci.mk@g' package/luci-app-diskman/Makefile
-
-## luci-app-filetransfer
-#git clone https://github.com/cddcx/luci-app-filetransfer.git package/luci-app-filetransfer
-#sed -i 's@../../luci.mk@$(TOPDIR)/feeds/luci/luci.mk@g' package/luci-app-filetransfer/Makefile
-# luci-app-filetransfer依赖luci-lib-fs
-#git clone https://github.com/cddcx/luci-lib-fs.git package/luci-lib-fs
-
-## luci-app-v2raya
-#git clone https://github.com/v2rayA/v2raya-openwrt package/v2raya-openwrt
-#rm -rf package/v2raya-openwrt/{v2raya,xray-core}
 
 ## luci-app-homeproxy
 git clone https://github.com/immortalwrt/homeproxy package/homeproxy           ####### homeproxy的默认版本(二选一)
@@ -71,16 +25,3 @@ git clone https://github.com/xiaorouji/openwrt-passwall-packages package/passwal
 rm -rf package/passwall2/{chinadns-ng,dns2socks,dns2tcp,hysteria,ipt2socks,microsocks,naiveproxy,shadowsocks-rust,shadowsocksr-libev,simple-obfs,sing-box,tcping,trojan-plus,trojan,tuic-client,v2ray-core,v2ray-geodata,v2ray-plugin,xray-core,xray-plugin}
 merge_package v5 https://github.com/sbwml/openwrt_helloworld openwrt_helloworld package/passwall2/chinadns-ng dns2socks dns2tcp hysteria ipt2socks microsocks naiveproxy shadowsocks-rust shadowsocksr-libev simple-obfs sing-box tcping trojan-plus trojan tuic-client v2ray-core v2ray-geodata v2ray-plugin xray-core xray-plugin
 merge_package main https://github.com/xiaorouji/openwrt-passwall2 openwrt-passwall2 package/luci-app-passwall2
-
-## luci-app-passwall2
-#mkdir -p luci-app-passwall2
-#git_clone_path main https://github.com/xiaorouji/openwrt-passwall2 luci-app-passwall2
-#cp -rf luci-app-passwall2 package/luci-app-passwall2
-#rm -rf luci-app-passwall2
-#git clone https://github.com/xiaorouji/openwrt-passwall-packages package/passwall2
-#svn checkout https://github.com/xiaorouji/openwrt-passwall2/trunk/luci-app-passwall2 package/luci-app-passwall2
-
-## luci-app-openclash
-#git_clone_path master https://github.com/vernesong/OpenClash luci-app-openclash
-#cp -rf luci-app-openclash package/luci-app-openclash
-#rm -rf luci-app-openclash
