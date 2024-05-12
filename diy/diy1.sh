eaebb2e32cf8b2781ee89470e58a8182b39a45e0
cddcx <84568016+cddcx@users.noreply.github.com>
2024-05-12 21:59:13 +0800

    Update diy1.sh

--- diy/diy1.sh
+++ diy/diy1.sh
@@ -23,5 +23,5 @@ sed -i "s@ImmortalWrt proxy@OpenWrt proxy@g" package/homeproxy/htdocs/luci-stati
 ## luci-app-passwall2
 git clone https://github.com/xiaorouji/openwrt-passwall-packages package/passwall2
 rm -rf package/passwall2/{chinadns-ng,dns2socks,dns2tcp,hysteria,ipt2socks,microsocks,naiveproxy,shadowsocks-rust,shadowsocksr-libev,simple-obfs,sing-box,tcping,trojan-plus,trojan,tuic-client,v2ray-core,v2ray-geodata,v2ray-plugin,xray-core,xray-plugin}
-merge_package v5 https://github.com/sbwml/openwrt_helloworld openwrt_helloworld package/passwall2/chinadns-ng dns2socks dns2tcp hysteria ipt2socks microsocks naiveproxy shadowsocks-rust shadowsocksr-libev simple-obfs sing-box tcping trojan-plus trojan tuic-client v2ray-core v2ray-geodata v2ray-plugin xray-core xray-plugin
-merge_package main https://github.com/xiaorouji/openwrt-passwall2 openwrt-passwall2 package/luci-app-passwall2
+merge_package v5 https://github.com/sbwml/openwrt_helloworld  package/openwrt_helloworld package/passwall2/chinadns-ng dns2socks dns2tcp hysteria ipt2socks microsocks naiveproxy shadowsocks-rust shadowsocksr-libev simple-obfs sing-box tcping trojan-plus trojan tuic-client v2ray-core v2ray-geodata v2ray-plugin xray-core xray-plugin
+merge_package main https://github.com/xiaorouji/openwrt-passwall2 package/openwrt-passwall2 package/luci-app-passwall2
