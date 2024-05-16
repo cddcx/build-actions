efab6dc2f86514896fa19930f6d626edb7b01110
cddcx <84568016+cddcx@users.noreply.github.com>
2024-05-16 21:47:36 +0800

    Update diy5.sh

--- diy/diy5.sh
+++ diy/diy5.sh
@@ -125,11 +125,13 @@ find package/*/ -maxdepth 2 -path "*/Makefile" | xargs -i sed -i 's/..\/..\/lang
 find package/*/ -maxdepth 2 -path "*/Makefile" | xargs -i sed -i 's/PKG_SOURCE_URL:=@GHREPO/PKG_SOURCE_URL:=https:\/\/github.com/g' {}
 find package/*/ -maxdepth 2 -path "*/Makefile" | xargs -i sed -i 's/PKG_SOURCE_URL:=@GHCODELOAD/PKG_SOURCE_URL:=https:\/\/codeload.github.com/g' {}
 
-# kiddin9的patches补丁
+# kiddin9的common及x86_64补丁
 merge_package master https://github.com/kiddin9/OpenWrt_x86-r2s-r4s-r5s-N1 devices devices/common devices/x86_64
+rm -rf devices/common/diy.sh
 rm -rf devices/common/patches/{targets.patch,luci_mk.patch}
 #merge_package master https://github.com/kiddin9/OpenWrt_x86-r2s-r4s-r5s-N1 devices/x86_64 devices/x86_64/patches
-rm -rf devices/x86_64/Intel_gpu.patch
+rm -rf devices/x86_64/diy.sh
+rm -rf devices/x86_64/patche/Intel_gpu.patch
 #cp -rn devices/common/patches devices/x86_64/
 #if [ -n "$(ls -A devices/x86_64/*.bin.patch 2>/dev/null)" ]; then
         #git apply devices/x86_64/patches/*.bin.patch
