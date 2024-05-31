951a8d498ebaece6eec28df97e52b9892d734118
cddcx <84568016+cddcx@users.noreply.github.com>
2024-05-31 21:32:40 +0800

    Update diy3.sh

--- diy/diy3.sh
+++ diy/diy3.sh
@@ -106,15 +106,15 @@ sed -i "s/DEFAULT_PACKAGES.router:=/DEFAULT_PACKAGES.router:=default-settings-ch
 
 ## 修改target/linux/x86/Makefile
 #sed -i 's/DEFAULT_PACKAGES += /DEFAULT_PACKAGES += luci-app-passwall2 luci-app-ttyd luci-app-udpxy /g' target/linux/x86/Makefile
-sed -i 's/DEFAULT_PACKAGES += /DEFAULT_PACKAGES += luci-app-upnp luci-app-udpxy luci-app-homeproxy luci-app-passwall2 /g' target/linux/x86/Makefile
+sed -i 's/DEFAULT_PACKAGES += /DEFAULT_PACKAGES += luci-ssl px5g-mbedtls luci-app-upnp luci-app-udpxy luci-app-homeproxy luci-app-passwall2 /g' target/linux/x86/Makefile
 
 ## 删除
 rm -rf feeds/luci/applications/{luci-app-v2raya,luci-app-shadowsocks-libev}
 rm -rf feeds/packages/net/{v2raya,microsocks,sing-box,shadowsocks-libev,v2ray-core,v2ray-geodata,xray-core}
 
 # haproxy
-rm -rf feeds/packages/net/haproxy
-merge_package master https://github.com/immortalwrt/packages feeds/packages/net net/haproxy
+#rm -rf feeds/packages/net/haproxy
+#merge_package master https://github.com/immortalwrt/packages feeds/packages/net net/haproxy
 
 # curl/8.5.0 - fix passwall `time_pretransfer` check
 #rm -rf feeds/packages/net/curl
