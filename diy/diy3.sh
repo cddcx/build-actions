388bfedccd125f37182d7030cb7e4bd1a5a5294d
cddcx <84568016+cddcx@users.noreply.github.com>
2024-06-19 21:49:31 +0800

    Update diy3.sh

--- diy/diy3.sh
+++ diy/diy3.sh
@@ -98,13 +98,11 @@ sed -i 's/DEFAULT_PACKAGES += /DEFAULT_PACKAGES += luci-app-upnp luci-app-udpxy 
 # 替换curl
 rm -rf feeds/packages/net/curl
 merge_package curl https://github.com/Ra2-IFV/packages feeds/packages/net net/curl
-
-
-curl_ver=$(cat feeds/packages/net/curl/Makefile | grep -i "PKG_VERSION:=" | awk 'BEGIN{FS="="};{print $2}' | awk 'BEGIN{FS=".";OFS="."};{print $1,$2}')
-if ((`expr $curl_ver \<= 8.8`)); then
-	echo "替换curl版本"
-	curl -s https://github.com/openwrt/packages/pull/24414/commits/2716f264363c3ab337ad1cdba522667fea45c93a.patch | patch -p1
-fi
+#curl_ver=$(cat feeds/packages/net/curl/Makefile | grep -i "PKG_VERSION:=" | awk 'BEGIN{FS="="};{print $2}' | awk 'BEGIN{FS=".";OFS="."};{print $1,$2}')
+#if ((`expr $curl_ver \<= 8.8`)); then
+	#echo "替换curl版本"
+	#curl -s https://github.com/openwrt/packages/pull/24414/commits/2716f264363c3ab337ad1cdba522667fea45c93a.patch | patch -p1
+#fi
 
 ## 删除
 rm -rf feeds/luci/applications/{luci-app-v2raya,luci-app-shadowsocks-libev}
