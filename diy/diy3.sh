e00d91a481f0b418e9e8fb9a3ce2a70250d81ae8
cddcx <84568016+cddcx@users.noreply.github.com>
2024-11-16 16:35:22 +0800

    Update diy3.sh

--- diy/diy3.sh
+++ diy/diy3.sh
@@ -153,7 +153,7 @@ CONFIG_PACKAGE_kmod-xdp-sockets-diag=y
 # kselftests-bpf
 #curl -s https://$mirror/openwrt/patch/packages-patches/kselftests-bpf/Makefile > package/devel/kselftests-bpf/Makefile
 rm -rf package/devel/kselftests-bpf/Makefile
-merge_package master https://github.com/sbwml/r4s_build_script package/devel/kselftests-bpf/Makefile openwrt/patch/packages-patches/kselftests-bpf/Makefile
+merge_package master https://github.com/sbwml/r4s_build_script package/devel openwrt/patch/packages-patches/kselftests-bpf
 
 # 拷贝自定义文件
 #if [ -n "$(ls -A "${GITHUB_WORKSPACE}/immortalwrt/diy" 2>/dev/null)" ]; then
