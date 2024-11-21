ff0f0468230a0ec407e123218222259df5ac3c5e
cddcx <84568016+cddcx@users.noreply.github.com>
2024-11-21 22:06:49 +0800

    Update diy2.sh

--- diy/diy2.sh
+++ diy/diy2.sh
@@ -161,6 +161,10 @@ CONFIG_PACKAGE_kmod-xdp-sockets-diag=y
 CONFIG_KERNEL_CC="clang"
 CONFIG_EXTRA_OPTIMIZATION=""
 # CONFIG_PACKAGE_kselftests-bpf is not set
+
+# Link time optimization
+CONFIG_USE_GC_SECTIONS=y
+CONFIG_USE_LTO=y
 ' >>  ./.config
 
 ### clang
