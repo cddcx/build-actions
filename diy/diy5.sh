5eaa71dd662e954d820f1201207749946ab07c42
cddcx <84568016+cddcx@users.noreply.github.com>
2024-12-12 15:45:23 +0800

    Update diy5.sh

--- diy/diy5.sh
+++ diy/diy5.sh
@@ -193,21 +193,20 @@ merge_package master https://github.com/sbwml/r4s_build_script package/devel ope
 
 ### clang
 # xtables-addons module
-rm -rf feeds/packages/net/xtables-addons
-git clone https://$github/sbwml/kmod_packages_net_xtables-addons feeds/packages/net/xtables-addons
+#rm -rf feeds/packages/net/xtables-addons
+#git clone https://$github/sbwml/kmod_packages_net_xtables-addons feeds/packages/net/xtables-addons
 # netatop
-sed -i 's/$(MAKE)/$(KERNEL_MAKE)/g' feeds/packages/admin/netatop/Makefile
-curl -s $mirror/openwrt/patch/packages-patches/clang/netatop/900-fix-build-with-clang.patch > feeds/packages/admin/netatop/patches/900-fix-build-with-clang.patch
-cp -rf package-patch/clang/netatop/900-fix-build-with-clang.patch feeds/packages/admin/netatop/patches/900-fix-build-with-clang.patch
+#sed -i 's/$(MAKE)/$(KERNEL_MAKE)/g' feeds/packages/admin/netatop/Makefile
+#curl -s $mirror/openwrt/patch/packages-patches/clang/netatop/900-fix-build-with-clang.patch > feeds/packages/admin/netatop/patches/900-fix-build-with-clang.patch
+#cp -rf package-patch/clang/netatop/900-fix-build-with-clang.patch feeds/packages/admin/netatop/patches/900-fix-build-with-clang.patch
 # dmx_usb_module
-rm -rf feeds/packages/libs/dmx_usb_module
-git clone https://$gitea/sbwml/feeds_packages_libs_dmx_usb_module feeds/packages/libs/dmx_usb_module
+#rm -rf feeds/packages/libs/dmx_usb_module
+#git clone https://$gitea/sbwml/feeds_packages_libs_dmx_usb_module feeds/packages/libs/dmx_usb_module
 # macremapper
-#curl -s $mirror/openwrt/patch/packages-patches/clang/macremapper/100-macremapper-fix-clang-build.patch | patch -p1
-patch -p1 -f < package-patch/clang/macremapper/100-macremapper-fix-clang-build.patch
+#patch -p1 -f < package-patch/clang/macremapper/100-macremapper-fix-clang-build.patch
 # coova-chilli module
-rm -rf feeds/packages/net/coova-chilli
-git clone https://$github/sbwml/kmod_packages_net_coova-chilli feeds/packages/net/coova-chilli
+#rm -rf feeds/packages/net/coova-chilli
+#git clone https://$github/sbwml/kmod_packages_net_coova-chilli feeds/packages/net/coova-chilli
 
 # 删除patch
 rm -rf package-patch
