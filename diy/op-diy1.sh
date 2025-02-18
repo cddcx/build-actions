c2588cc4cab896437a4cfb7ea41d2826c9e1e175
cddcx <84568016+cddcx@users.noreply.github.com>
2025-02-18 16:12:52 +0800

    Update op-diy1.sh

--- diy/op-diy1.sh
+++ diy/op-diy1.sh
@@ -82,12 +82,12 @@ git clone https://github.com/QiuSimons/luci-app-daed package/dae
 #merge_package main https://github.com/kenzok8/small-package package/helloworld libcron
 merge_package openwrt-24.10 https://github.com/immortalwrt//packages package/libs libs/libcron
 
-# luci-app-mihomo
+# luci-app-nikki
 merge_package main https://github.com/nikkinikki-org/OpenWrt-nikki package/helloworld luci-app-nikki
 merge_package main https://github.com/nikkinikki-org/OpenWrt-nikki package/helloworld nikki
 
 # bpf - add host clang-15/18/20 support
-sed -i 's/command -v clang/command -v clang clang-15 clang-18 clang-20/g' include/bpf.mk
+sed -i 's/command -v clang/command -v clang clang-18 clang-20/g' include/bpf.mk
 
 export mirror=raw.githubusercontent.com/sbwml/r4s_build_script/master
 export gitea=git.cooluc.com
