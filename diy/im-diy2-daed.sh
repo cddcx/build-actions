56af0553e03166977c944c0f98b5d8465ed76e09
cddcx <84568016+cddcx@users.noreply.github.com>
2025-04-03 15:02:39 +0800

    Update im-diy2-daed.sh

--- diy/im-diy2-daed.sh
+++ diy/im-diy2-daed.sh
@@ -89,7 +89,7 @@ sed -i 's/procd_set_param stderr 1/procd_set_param stderr 0/g' feeds/packages/ut
 # 修改include/target.mk
 sed -i "s/DEFAULT_PACKAGES.router:=/DEFAULT_PACKAGES.router:=default-settings-chn luci-app-opkg /" include/target.mk
 sed -i "s/kmod-nft-offload/kmod-nft-offload kmod-nft-tproxy/" include/target.mk
-sed -i '#autocore \#d' include/target.mk
+#sed -i '#autocore \#d' include/target.mk
 sed -i '#luci-app-package-manager \#d' include/target.mk
 
 # 修改target/linux/x86/Makefile
