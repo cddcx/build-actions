0a92910caea041cceaf892d5c8a607b5b9fb45f5
cddcx <84568016+cddcx@users.noreply.github.com>
2025-04-26 12:48:02 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,3 +18,15 @@
 # 添加一个 feed 源
 # echo "src-git helloworld https://github.com/fw876/helloworld.git" >> "feeds.conf.default"
 # echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+
+# luci-app-daed
+#git clone https://github.com/QiuSimons/luci-app-daed package/dae
+# 依赖
+#merge_package openwrt-24.10 https://github.com/immortalwrt//packages package/libs libs/libcron
+
+# luci-app-nikki
+#merge_package main https://github.com/nikkinikki-org/OpenWrt-nikki package/helloworld luci-app-nikki
+#merge_package main https://github.com/nikkinikki-org/OpenWrt-nikki package/helloworld nikki
+
+# bpf - add host clang-15/18/20 support
+sed -i 's/command -v clang/command -v clang clang-15 clang-18 clang-20/g' include/bpf.mk
