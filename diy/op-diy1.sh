dd7d1c597025902672baf2dd66440b4b9a2dc1e1
cddcx <84568016+cddcx@users.noreply.github.com>
2025-04-29 11:04:45 +0800

    Update op-diy1.sh

--- diy/op-diy1.sh
+++ diy/op-diy1.sh
@@ -58,7 +58,7 @@ echo "========================="
 #git clone https://github.com/sirpdboy/luci-app-advancedplus package/luci-app-advancedplus
 
 # uci-app-partexp 一键自动格式化分区、扩容、自动挂载插件
-git clone https://github.com/sirpdboy/luci-app-partexp.git package/luci-app-partexp
+#git clone https://github.com/sirpdboy/luci-app-partexp.git package/luci-app-partexp
 
 # DPDK & NUMACTL
 merge_package master https://github.com/sbwml/r4s_build_script package/new openwrt/patch/dpdk/dpdk
