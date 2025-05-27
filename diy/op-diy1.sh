e90e1e3c71eaa6ef3d2eb0dc4b8f3678e52e87e3
cddcx <84568016+cddcx@users.noreply.github.com>
2025-05-27 10:00:57 +0800

    Update op-diy1.sh

--- diy/op-diy1.sh
+++ diy/op-diy1.sh
@@ -86,7 +86,7 @@ merge_package main https://github.com/nikkinikki-org/OpenWrt-nikki package/hello
 merge_package main https://github.com/nikkinikki-org/OpenWrt-nikki package/helloworld nikki
 
 # bpf - add host clang-15/18/20 support
-sed -i 's/clang-13/clang-15 clang-18 clang-20/g' include/bpf.mk
+sed -i 's/find-llvm-tool,clang/find-llvm-tool,clang clang-18 clang-15/g' include/bpf.mk
 
 # luci-app-passwall
 #merge_package main https://github.com/xiaorouji/openwrt-passwall package luci-app-passwall
