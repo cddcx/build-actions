ee1d9ec7883f4720b4cc07fccb958f57a16f4dd5
cddcx <84568016+cddcx@users.noreply.github.com>
2026-03-30 20:43:31 +0800

    Update op-diy1.sh

--- diy/op-diy1.sh
+++ diy/op-diy1.sh
@@ -55,8 +55,8 @@ echo "src-git momo https://github.com/nikkinikki-org/OpenWrt-momo.git;main" >> "
 # luci-app-daed及依赖
 merge_package openwrt-24.10 https://github.com/immortalwrt//packages package/libs libs/libcron
 git clone https://github.com/sbwml/package_kernel_vmlinux-btf package/kernel/vmlinux-btf
-#merge_package v5 https://github.com/sbwml/openwrt_helloworld package/dae daed luci-app-daed
-git clone https://github.com/QiuSimons/luci-app-daed package/dae
+merge_package v5 https://github.com/sbwml/openwrt_helloworld package/dae daed luci-app-daed
+#git clone https://github.com/QiuSimons/luci-app-daed package/dae
 
 # luci-app-openclash
 merge_package master https://github.com/vernesong/OpenClash package luci-app-openclash
