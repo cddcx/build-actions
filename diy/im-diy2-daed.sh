dbf670099f26d5d46d63e488832eadfc1926d576
cddcx <84568016+cddcx@users.noreply.github.com>
2026-04-20 16:36:12 +0800

    Update im-diy2-daed.sh

--- diy/im-diy2-daed.sh
+++ diy/im-diy2-daed.sh
@@ -90,11 +90,14 @@ sed -i 's/procd_set_param stderr 1/procd_set_param stderr 0/g' feeds/packages/ut
 sed -i "s/DEFAULT_PACKAGES.router:=/DEFAULT_PACKAGES.router:=default-settings-chn vmlinux-btf /" include/target.mk
 sed -i "s/kmod-nft-offload/kmod-nft-offload kmod-nft-tproxy/" include/target.mk
 #sed -i '#autocore \#d' include/target.mk
-sed -i '#luci-app-cpufreq \#d' include/target.mk
 
 # 修改target/linux/x86/Makefile
 #sed -i 's/fdisk/luci-app-udpxy/g' target/linux/x86/Makefile
 sed -i 's/automount/luci-app-nikki/g' target/linux/x86/Makefile
+
+# 删除luci-app-cpufreq
+sed -i '#luci-app-cpufreq \#d' include/target.mk
+rm -rf package/emortal/cpufreq
 
 ## 删除软件
 rm -rf feeds/luci/applications/luci-app-adguardhome
